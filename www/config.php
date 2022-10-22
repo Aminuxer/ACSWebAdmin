7cb8c327c7f90cf45d11ab654ea4d7eb246f2252
Amin <Aminuxer@users.noreply.github.com>
2022-10-22 22:39:09 +0300

    Add files via upload

---
+++ www/config.php
@@ -0,0 +1,22 @@
+<?php
+
+   /* MySQL DB Connect options */
+
+$db_host = 'localhost';
+$db_name = 'z5r';
+$db_user = 'z5r';
+$db_pswd = 'z5r';
+
+/* Change this value to any random string after installation */
+$sess_secret_salt = 'this string must be changed to anywhere';
+
+$debug = 1;                    // 0 - no debug; 1 - short log; 2 - detail log; 3 - very detailed log ;
+$debug_log = '/tmp/z5r.txt';   // log path
+
+/* Localization. Default = ru, available = en / de or other file like ??.php from localization dir */
+$localization = 'ru';
+
+# ============================
+require("func.php");
+
+?>
